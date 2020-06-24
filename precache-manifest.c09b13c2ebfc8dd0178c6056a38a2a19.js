833b13122f016a19b9e8948f1dcc5d5c1c3082af
Duwan Peña <duwanp2112@gmail.com>
2020-06-24 18:41:38 -0430

    Updates

---
+++ precache-manifest.c09b13c2ebfc8dd0178c6056a38a2a19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073354fac49ad9b4db223eaf000a8a9d",
+    "url": "/Primer-Proyect/index.html"
+  },
+  {
+    "revision": "5cc2676b5bb2eb88f450",
+    "url": "/Primer-Proyect/static/js/2.499151ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Primer-Proyect/static/js/2.499151ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee793bfe3d0327eaf2e",
+    "url": "/Primer-Proyect/static/js/main.db9cdbe0.chunk.js"
+  },
+  {
+    "revision": "3abc0f203c325af42061",
+    "url": "/Primer-Proyect/static/js/runtime-main.4e7f3d3b.js"
+  }
+]);
